d9aa511ab7045d4479aa86b518263e0d6fa683b5
nailyasarkarova <nailyasarkarova001@gmail.com>
2022-12-08 16:17:22 +0400

    Registration Scenario

---
+++ LMSAutoTesting/Features/Registration.feature
@@ -0,0 +1,14 @@
+﻿Feature: Registration
+@registration
+Scenario: Registrate new student
+	Given first name is <first name>
+	And last name is <last name> 
+	And age is <age>
+	And email is <email>
+	And password is антонмантон123
+	And phone number is +712345678
+	When new usser is registrated
+	Then student registrated
+	Examples: 
+	| first name | last name | age | email | password | phone number |registrated  | 
+	| Anton | Efremenkov | 30 | anton1@mail.ru | антонмантон123 |+712345678  |registrated  | 
---
+++ LMSAutoTesting/StepDefinitions/RegistrationStepDefinition.cs
@@ -0,0 +1,26 @@
+﻿
+namespace LMSAutoTesting.StepDefinitions
+{
+    [Binding]
+    public sealed class RegistrationStepDefinitions
+    {
+        [Given("last name is Anton (.*)")]
+        public void GivenFirstNameIs(string number)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given("last name is Efremenkov (.*)")]
+        public void GivenLastNameIs(string number)
+        {
+            throw new PendingStepException();
+        }
+        [Given("age is 30 (.*)")]
+        public void GivenAgeIs(int number)
+        {
+            throw new PendingStepException();
+        }
+        [Given("email is anton1@mail.ru (.*)")]
+        public void GivenEmailIs(int number)
+        {
+            throw new PendingStepException();
