2857adf14b04ab79f7645757df37d2ef9189c826
sayojj <aleksbaydikov@gmail.com>
2022-12-14 10:46:29 +0400

    ClassAdd

---
+++ LMSAutoTesting/Support/DriverStorage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMSAutoTesting.Support
+{
+    public class DriverStorage
+    {
+        public WebDriver driver { get; set; }
+
+        private static DriverStorage _driverStorage;
+
+        private DriverStorage()
+        {
+        }
+
+        public static DriverStorage GetInstance()
+        {
+            if (_driverStorage is null)
+            {
+                _driverStorage = new DriverStorage();
+            }
+            return _driverStorage;
+        }
+    }
+}
