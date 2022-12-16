a11b14ca76f3bd53ce2fc9573f89cf9514fc73e0
GesS016 <gusikasadov@gmail.com>
2022-12-17 02:21:03 +0400

    authPage methods added

--- LMSAutoTesting/Pages/AuthPage.cs
+++ LMSAutoTesting/Pages/AuthPage.cs
@@ -1,4 +1,5 @@
 ﻿using LMSAutoTesting.Support;
+using OpenQA.Selenium.Interactions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +10,26 @@ namespace LMSAutoTesting.Pages
 {
     public class AuthPage : AbstractPage
     {
-        public IWebElement eMailBox => _driver.FindElement(By.XPath(@"//input[@class = 'form-input']"));
+        public IWebElement emailBox => _driver.FindElement(By.XPath(@"//input[@class = 'form-input']"));
         public IWebElement passwordBox => _driver.FindElement(By.XPath(@"//input[@class = 'form-input custom-password']"));
+        public IWebElement buttonEnter => _driver.FindElement(By.XPath(@"//button[@class = 'sc-bczRLJ iJvUkY btn btn-fill flex-container']"));
         
         public override void Open()
         {
             _driver.Navigate().GoToUrl(Urls.AuthPage);
+        }
+
+        public void EnterEmail(string email, string password)
+        {
+            Actions actions= new Actions(_driver);
+            actions.Click(emailBox).SendKeys(email).Build().Perform();
+            actions.Click(passwordBox).SendKeys(password).Build().Perform();
+        }
+
+        public void PressEnter()
+        {
+            Actions actions = new Actions(_driver);
+            actions.Click(buttonEnter).Perform();
         }
     }
 }
