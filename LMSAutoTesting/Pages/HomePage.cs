7cae0be3c07371d41faa5501af132c7fa88ffeb0
sayojj <aleksbaydikov@gmail.com>
2022-12-17 05:42:02 +0400

    HomePage created

---
+++ LMSAutoTesting/Pages/HomePage.cs
@@ -0,0 +1,20 @@
+﻿using LMSAutoTesting.Support;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMSAutoTesting.Pages
+{
+    public class HomePage : AbstractPage
+    {
+        public IWebElement Logout => _driver.FindElement(By.XPath(@"/html/body/div/div/aside/div/div[3]/button/span"));
+        public IWebElement buttonSetting => _driver.FindElement(By.XPath(@"/html/body/div/div/aside/div/nav/a[7]/span"));
+
+        public override void Open()
+        {
+            _driver.Navigate().GoToUrl(Urls.HomePage);
+        }
+    }
+}
--- LMSAutoTesting/Pages/SettingsPage.cs
+++ LMSAutoTesting/Pages/SettingsPage.cs
@@ -10,7 +10,6 @@ namespace LMSAutoTesting.Pages
 {
     public class SettingsPage : AbstractPage
     {
-        public IWebElement buttonSetting => _driver.FindElement(By.XPath(@"/html/body/div/div/aside/div/nav/a[7]/span"));
         public IWebElement passwordBox => _driver.FindElement(By.XPath(@"/html/body/div/div/main/div[1]/form/div[2]/div[1]/div/a/svg"));
 
         public override void Open()
