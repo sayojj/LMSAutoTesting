b2ebe7b78e6d20e9fa60e5679f036446991fc95e
sayojj <aleksbaydikov@gmail.com>
2022-12-17 04:33:26 +0400

    Setting Page created

---
+++ LMSAutoTesting/Pages/Settings.cs
@@ -0,0 +1,20 @@
+﻿using LMSAutoTesting.Support;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMSAutoTesting.Pages
+{
+    public class Settings : AbstractPage
+    {
+        public IWebElement passwordBox => _driver.FindElement(By.XPath(@"//div[@class='form-input']));
+
+        public override void Open()
+        {
+            _driver.Navigate().GoToUrl(Urls.AuthPage);
+        }
+    }
+}
