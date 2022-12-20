e26d23abccc23ca3fea2db1d7f00ad75872febec
GesS016 <gusikasadov@gmail.com>
2022-12-21 01:54:02 +0400

    Scenario created

---
+++ LMSAutoTesting/Features/AddGroupMember.feature
@@ -0,0 +1,16 @@
+﻿Feature: AddGroupMember
+
+A short summary of the feature
+
+@tag1
+Scenario: Add member to group
+	Given Open Auth web page
+	Given Ignore security warning
+	When Enter email "[string]"
+	And Enter password "[string]"
+	And Click enter button
+	Given Group created
+	When press groups on HomePage
+	And Choose group from list of groups
+	And edit students of group list
+	Then page of students should be opened
--- LMSAutoTesting/Pages/HomePage.cs
+++ LMSAutoTesting/Pages/HomePage.cs
@@ -9,16 +9,14 @@ namespace LMSAutoTesting.Pages
     {
         public IWebElement Logout => _driver.FindElement(By.XPath(@"/html/body/div/div/aside/div/div[3]/button/span"));
         public IWebElement ButtonSettings => _driver.FindElement(By.XPath(@"/html/body/div/div/aside/div/nav/a[7]/span"));
-        //public IWebElement Username => _driver.FindElement(By.XPath(@"//span[@class='avatar-name transition-styles']"));
         public IWebElement Username
         {
             get
             {
                 WebDriverWait driverWait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
-                return driverWait.Until(ExpectedConditions.ElementExists((By.XPath(@"//span[@class='avatar-name transition-styles']")));
+                return driverWait.Until(ExpectedConditions.ElementExists((By.XPath(@"//span[@class='avatar-name transition-styles']"))));
             }
         }
-        //_driver.FindElement(By.XPath(@"//span[@class='avatar-name transition-styles']"));
 
         public override void Open()
         {
