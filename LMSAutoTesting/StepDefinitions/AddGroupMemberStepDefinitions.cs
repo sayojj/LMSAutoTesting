c905d1cd2be959f3c3ccb1d93eb257258847d1a2
GesS016 <gusikasadov@gmail.com>
2022-12-21 02:11:47 +0400

    Definitions created, button added

---
+++ LMSAutoTesting/StepDefinitions/AddGroupMemberStepDefinitions.cs
@@ -0,0 +1,33 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace LMSAutoTesting.StepDefinitions
+{
+    [Binding]
+    public class AddGroupMemberStepDefinitions
+    {
+        [When(@"press groups on HomePage")]
+        public void WhenPressGroupsOnHomePage()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Choose group from list of groups")]
+        public void WhenChooseGroupFromListOfGroups()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"edit students of group list")]
+        public void WhenEditStudentsOfGroupList()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"page of students should be opened")]
+        public void ThenPageOfStudentsShouldBeOpened()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
