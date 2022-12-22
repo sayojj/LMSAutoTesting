1e2a5bd0d1ece5fe4da5600920517ab353b1b4e1
GesS016 <gusikasadov@gmail.com>
2022-12-22 13:15:46 +0400

    TimeSpan added

--- LMSAutoTesting/Pages/HomePage.cs
+++ LMSAutoTesting/Pages/HomePage.cs
@@ -8,7 +8,14 @@ namespace LMSAutoTesting.Pages
     public class HomePage : AbstractPage
     {
         public IWebElement Logout => _driver.FindElement(By.XPath(@"/html/body/div/div/aside/div/div[3]/button/span"));
-        public IWebElement ButtonGroups => _driver.FindElement(By.XPath(@"//a[@href='/groups']"));
+        public IWebElement ButtonGroups
+        {
+            get
+            {
+                WebDriverWait driverWait=new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
+                return driverWait.Until(ExpectedConditions.ElementExists(By.XPath("//a[@href='/groups']")));
+            }
+        }
         public IWebElement ButtonStudentsList => _driver.FindElement(By.XPath(@"//a[@href='/students-list']"));
         public IWebElement ButtonSettings => _driver.FindElement(By.XPath(@"/html/body/div/div/aside/div/nav/a[7]/span"));
         public IWebElement Username
