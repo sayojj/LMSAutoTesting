b15a41c5e20c92d36fd197a8edc97d4d2e2d1a6f
GesS016 <gusikasadov@gmail.com>
2022-12-26 01:31:23 +0400

    WebClient and RegistrationRequestModel added

---
+++ LMSAutoTesting/RequestModels/RegistrationRequestModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace LMSAutoTesting.RequestModels
+{
+    public class RegistrationRequestModel
+    {
+            [JsonPropertyName("firstName")]
+            public string firstName { get; set; }
+
+            [JsonPropertyName("lastName")]
+            public string lastName { get; set; }
+
+            [JsonPropertyName("patronymic")]
+            public string patronymic { get; set; }
+
+            [JsonPropertyName("email")]
+            public string email { get; set; }
+
+            [JsonPropertyName("username")]
+            public string username { get; set; }
+
+            [JsonPropertyName("password")]
+            public string password { get; set; }
+
+            [JsonPropertyName("city")]
+            public string city { get; set; }
+
+            [JsonPropertyName("birthDate")]
+            public string birthDate { get; set; }
+
+            [JsonPropertyName("gitHubAccount")]
+            public string gitHubAccount { get; set; }
+
+            [JsonPropertyName("phoneNumber")]
+            public string phoneNumber { get; set; }
+    }
+}
