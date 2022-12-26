2b54f741d4310576ad5bca43ecbdbfe3c04d68eb
nailyasarkarova <nailyasarkarova001@gmail.com>
2022-12-26 17:51:26 +0400

    Auth request models

---
+++ LMSAutoTesting/RequestModels/AuthRequestModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace LMSAutoTesting.RequestModels
+{
+    public class AuthRequestModel
+    {
+            [JsonPropertyName("email")]
+            public string email { get; set; }
+
+            [JsonPropertyName("password")]
+            public string password { get; set; }
+    }
+}
