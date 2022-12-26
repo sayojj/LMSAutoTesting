b79379994e2479ebbb7eeade054543e31f448f1b
nailyasarkarova <nailyasarkarova001@gmail.com>
2022-12-26 18:35:34 +0400

    Course request models

---
+++ LMSAutoTesting/RequestModels/CourseRequiestModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace LMSAutoTesting.RequestModels
+{
+    public class CourseRequiestModel
+    {
+        public class Root
+        {
+            [JsonPropertyName("name")]
+            public string name { get; set; }
+
+            [JsonPropertyName("description")]
+            public string description { get; set; }
+        }
+
+    }
+}
