5c4a6a2f9828928b287db4880e2de0037476cc53
GesS016 <gusikasadov@gmail.com>
2022-12-28 03:07:43 +0400

    DisableTestParallelization

--- LMSAutoTesting/ImplicitUsings.cs
+++ LMSAutoTesting/ImplicitUsings.cs
@@ -3,3 +3,4 @@ global using TechTalk.SpecFlow;
 global using Xunit;
 global using OpenQA.Selenium;
 global using OpenQA.Selenium.Chrome;
+[assembly: Xunit.CollectionBehavior(DisableTestParallelization = true)]
